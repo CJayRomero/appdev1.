e9a33e7a433af04b83bfab6cb57df4e682e1e4ff
CJayRomero <christianjayromero0101@gmail.com>
2025-10-04 10:59:22 +0800

    Add Projects component with sample project list

---
+++ appdev1-midterm-exam/src/components/Projects.jsx
@@ -0,0 +1,12 @@
+export default function Projects() {
+  return (
+    <section id="projects" className="projects">
+      <h2>Projects</h2>
+      <ul>
+        <li>Portfolio Website</li>
+        <li>React Todo App</li>
+        <li>Digital Art Showcase</li>
+      </ul>
+    </section>
+  );
+}
